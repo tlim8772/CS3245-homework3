b19c5ef3e079ae2313affe35afabab37d782a7e8
obrona <tlim8772@gmail.com>
2026-03-06 23:20:26 +0800

    add index helper

---
+++ src/index_helper.py
@@ -0,0 +1,57 @@
+import os
+import nltk
+from pathlib import Path
+from collections import Counter
+import itertools
+import math
+import pickle
+
+# documents_length: maps file name to the vector length
+# document_frequency: maps word to document frequency
+def index_helper(in_dir: str, out_dict: str, out_postings: str):
+    doc_lens: dict[str, float] = {}
+    doc_freqs: Counter[str] = Counter()
+    postings: dict[str, list[tuple[str,float]]] = {}
+    stemmer = nltk.stem.PorterStemmer()
+
+    for path in Path(in_dir).iterdir():
+        if not path.is_file() or not path.name.isdigit():
+            continue
+        
+        with path.open('r') as f:
+            counter: Counter[str] = Counter()
+            text = f.read()
+            sentences = nltk.sent_tokenize(text)
+            words = itertools.chain.from_iterable(map(lambda sentence: nltk.word_tokenize(sentence), sentences))
+            tokens = map(lambda word: stemmer.stem(word, to_lowercase=True), words)
+            for token in tokens:
+                counter[token] += 1
+            
+            sum = 0.0
+            for word, freq in counter.items():
+                doc_freqs[word] += 1
+                w = 1 + math.log10(freq)
+                sum += w**2
+                if word not in postings:
+                    postings[word] = []
+                postings[word].append((path.name, w))
+            doc_lens[path.name] = math.sqrt(sum)
+
+    with open(out_dict, 'wb') as dict_f, open(out_postings, 'wb') as postings_f:
+        pickle.dump(doc_lens, dict_f)
+        pickle.dump(doc_freqs, dict_f)
+        
+        offset_dict: dict[str, int] = {}
+        offset = 0
+        for word, posting_list in postings.items():
+            byte_obj = pickle.dumps(posting_list)
+            postings_f.write(byte_obj)
+            offset_dict[word] = offset
+            offset += len(byte_obj)
+        pickle.dump(offset_dict, dict_f)
+
+            
+
+
+        
+
---
+++ tests/index_helper_test.py
@@ -0,0 +1,35 @@
+import unittest
+from src.index_helper import *
+import shutil
+
+def create_in_dir(in_dir):
+    if os.path.exists(in_dir):
+        shutil.rmtree(in_dir)
+    os.makedirs(in_dir)
+    for i in range(4):
+        with open(f'{in_dir}/{i}', 'w') as f:
+            f.write('a b c')
+
+class Test(unittest.TestCase):
+    def test_index_helper(self):
+        in_dir = 'test_dir'
+        out_dict = 'out_dict'
+        out_postings = 'out_postings'
+        create_in_dir(in_dir)
+        index_helper(in_dir, out_dict, out_postings)
+
+        with open(out_dict, 'rb') as d, open(out_postings, 'rb') as p:
+            doc_lens = pickle.load(d)
+            doc_freqs = pickle.load(d)
+            doc_offsets = pickle.load(d)
+            self.assertEqual(doc_lens, {'0': math.sqrt(3), '1':math.sqrt(3), '2':math.sqrt(3), '3': math.sqrt(3)})
+            self.assertEqual(doc_freqs, {'a': 4, 'b': 4, 'c': 4})
+
+            offset = doc_offsets['b']
+            p.seek(offset)
+            lst = pickle.load(p)
+            self.assertEqual(set(lst), set([('0', 1.0), ('1', 1.0), ('2', 1.0), ('3', 1.0)]))
+
+        shutil.rmtree(in_dir)
+        os.remove(out_dict)
+        os.remove(out_postings)
